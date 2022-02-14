93cdc85c09b942aed8645f760279f588827e95f1
Numan Saady <numansaady@gmail.com>
2022-02-14 08:46:49 +0600

    string searc code added

--- string-search.js
+++ string-search.js
@@ -1,1 +1,56 @@
+const products = [
+  "Dell Laptop",
+  "Lenovo laptop",
+  "Toshiba laptop",
+  "Dell N95 Mobile phone",
+  "N73 Mobile phone",
+  "Dell Galaxy s9 Phone",
+  "Acer Laptop",
+];
 
+const searching = "phone";
+
+// indexOf with case sensitive
+// const output = [];
+// for (const product of products) {
+//   if (product.indexOf(searching) != -1) {
+//     output.push(product);
+//   }
+// }
+// console.log(output);
+
+// indexOf() with convert string to lower case
+// const output = [];
+// for (const product of products) {
+//   if (product.toLowerCase().indexOf(searching.toLowerCase()) != -1) {
+//     output.push(product);
+//   }
+// }
+// console.log(output);
+
+// includes() with convert string to lower case
+// const output = [];
+// for (const product of products) {
+//   if (product.toLowerCase().includes(searching.toLowerCase()) != -1) {
+//     output.push(product);
+//   }
+// }
+// console.log(output);
+
+// startsWith() with convert string to lower case
+// const output = [];
+// for (const product of products) {
+//   if (product.toLowerCase().startsWith(searching.toLowerCase())) {
+//     output.push(product);
+//   }
+// }
+// console.log(output);
+
+// endsWith() with convert string to lower case
+const output = [];
+for (const product of products) {
+  if (product.toLowerCase().endsWith(searching.toLowerCase())) {
+    output.push(product);
+  }
+}
+console.log(output);
