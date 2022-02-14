276cc0d5bc95076a43c84e1c5337ad2725ad8372
Numan Saady <numansaady@gmail.com>
2022-02-14 08:44:56 +0600

    case convert file added

---
+++ case-convert.js
@@ -0,0 +1,22 @@
+const userName = "Sabiha";
+const savedUserName = "sabiha";
+
+if (userName == savedUserName) {
+  console.log("User is exists");
+}
+
+// String Converted to Lower Case
+userName.toLowerCase;
+savedUserName.toLowerCase;
+
+if (userName.toLowerCase() == savedUserName.toLowerCase()) {
+  console.log("User is exists");
+}
+
+// String Converted to Upper Case
+userName.toUpperCase;
+savedUserName.toUpperCase;
+
+if (userName.toUpperCase() == savedUserName.toUpperCase()) {
+  console.log("User is exists");
+}
